54d6bed9478ba0a22b5a5833a2035f78725c883c
Petrus Prinsloo <petrus.prinsloo@gmail.com>
2017-07-22 00:02:59 +0200

    created project

---
+++ TestingGrounds/Intermediate/Build/Win64/UE4Editor/Inc/TestingGrounds/TestingGrounds.generated.dep.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "GeneratedCppIncludes.h"
+#ifndef TESTINGGROUNDS_TestingGroundsCharacter_generated_h
+	#include "TestingGroundsCharacter.h"
+#endif
+#ifndef TESTINGGROUNDS_TestingGroundsGameMode_generated_h
+	#include "TestingGroundsGameMode.h"
+#endif
+#ifndef TESTINGGROUNDS_TestingGroundsHUD_generated_h
+	#include "TestingGroundsHUD.h"
+#endif
+#ifndef TESTINGGROUNDS_TestingGroundsProjectile_generated_h
+	#include "TestingGroundsProjectile.h"
+#endif
