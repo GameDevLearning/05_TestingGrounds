66350ab749625c57f43e0171c766e6b0d9d488f2
Petrus Prinsloo <petrus.prinsloo@gmail.com>
2017-08-30 21:30:49 +0200

    Moved all logic out of blueprints into C++ (currently broken)

--- TestingGrounds/Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ TestingGrounds/Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -1,13 +1,28 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ChooseNextWaypoint.h"
+#include "AIController.h"
+#include "PatrollingGuard.h"
 #include "BehaviorTree/BlackboardComponent.h"
-
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
+	// get the patrol points
+	auto AIController = OwnerComp.GetAIOwner();
+	auto ControlledPawn = AIController->GetPawn();
+	auto PatrollingGaurd = Cast<APatrollingGuard>(ControlledPawn);
+	auto PatrolPoints = PatrollingGaurd->PatrolPointsCPP;
+
+	//set next waypoint 
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
 	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
+
+	// TODO protect against empty patrol points
+
+	// cycle the index
+	auto NextIndex = (Index + 1) % PatrolPoints.Num();
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
 	
 	UE_LOG(LogTemp, Warning, TEXT("Waypoint index: %i"), Index);
 
